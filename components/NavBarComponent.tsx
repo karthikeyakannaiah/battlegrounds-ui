75a2da095e4222cc233182ba621d1d6beef5554a
Karthikeya <karthikeya0011k@outlook.com>
2025-02-09 23:42:23 +0530

    Setup Layout

---
+++ components/NavBarComponent.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { AuthContext } from "../app/AuthContext";
+import { useContext } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getAvatarFallbackLetters, formatDisplayName } from "@/lib/utils";
+import Link from "next/link";
+
+export default function NavBarComponent() {
+  const context = useContext(AuthContext);
+  const user = context?.user;
+  return (
+    <div className="box-border m-5 flex justify-between items-center">
+        <div className="bg-slate-100 inline-block p-3 text-3xl my-3">Battle<span className="text-red-600">Grounds</span></div>
+        {
+          user && (
+            <>
+            <div className="links flex-row">
+                <Link href="/Tournaments"><span className="text-lg font-semibold hover:text-red-600 hover:border-b-2 hover:border-black">Tournaments</span></Link>
+            </div>
+            <div className="flex items-center space-x-4 p-4">
+              {/* Avatar */}
+              <Avatar className="w-12 h-12 border-2 border-slate-300 rounded-full overflow-hidden">
+                <AvatarImage src={user.photoURL || ""} alt="User Avatar" />
+                <AvatarFallback>{getAvatarFallbackLetters(user.displayName || "")}</AvatarFallback>
+              </Avatar>
+
+              {/* Welcome Text & Name */}
+              <div className="flex flex-col">
+                <span className="text-sm text-red-500">Welcome</span>
+                <span className="text-lg font-semibold">{formatDisplayName(user.displayName)}</span>
+              </div>
+            </div>
+            </>
+          )
+        }
+      </div>
+  )
+}
---
+++ lib/utils.ts
@@ -0,0 +1,31 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function getAvatarFallbackLetters(str: string = "") {
+const [first, second] = str.split(" ");
+
+let result = capitalizeFirstLetter(first.charAt(0));
+
+if (second) {
+  result += capitalizeFirstLetter(second.charAt(0));
+}
+
+return result;
+}
+
+export function formatDisplayName(displayName: string | null) {
+const firstName = (displayName || "").split(" ")[0];
+let result = firstName;
+if (firstName.length > 15) {
+  result = firstName.slice(0, 15) + "...";
+}
+return capitalizeFirstLetter(result);
+}
